c5d46ab76389b341569e60ac653893952eb04020
Dominik Toton <vaaadus@gmail.com>
2016-08-21 20:29:30 +0200

    added Timer class

---
+++ lib/Timer/Timer.cpp
@@ -0,0 +1,65 @@
+#include "Timer.h"
+
+Timer::Timer() {
+  for (int i = 0; i < MAX_TIMERS; i++)
+    functions[i] = 0;
+  lastUpdate = millis();
+}
+
+int8_t Timer::setTimeout(void (*function)(), unsigned long timeout) {
+  int8_t slot = findTimerSlot();
+  if (slot == -1)
+    return slot;
+
+  functions[slot] = function;
+  intervals[slot] = timeout;
+  currentIntervals[slot] = timeout;
+  repeat[slot] = false;
+  return slot;
+}
+
+int8_t Timer::setInterval(void (*function)(), unsigned long interval) {
+  int8_t slot = findTimerSlot();
+  if (slot == -1)
+    return slot;
+
+  functions[slot] = function;
+  intervals[slot] = interval;
+  currentIntervals[slot] = interval;
+  repeat[slot] = true;
+  return slot;
+}
+
+void Timer::deleteTimer(uint8_t slot) {
+  if (slot >= 0 && slot <= MAX_TIMERS)
+    functions[slot] = 0;
+}
+
+void Timer::update() {
+  unsigned int updateTime = millis() - lastUpdate;
+  lastUpdate = millis();
+
+  for (int i = 0; i < MAX_TIMERS; i++) {
+    if (functions[i] == 0)
+      continue;
+
+    if (currentIntervals[i] <= updateTime) {
+      functions[i]();
+      if (repeat[i]) {
+        currentIntervals[i] = intervals[i];
+      } else {
+        functions[i] = 0;
+      }
+    } else {
+      currentIntervals[i] -= updateTime;
+    }
+  }
+}
+
+int8_t Timer::findTimerSlot() {
+  for (int i = 0; i < MAX_TIMERS; i++) {
+    if (functions[i] == 0)
+      return i;
+  }
+  return -1;
+}
---
+++ lib/Timer/Timer.h
@@ -0,0 +1,28 @@
+#ifndef TIMER_H
+#define TIMER_H
+
+#include <Arduino.h>
+
+class Timer {
+public:
+  const static uint8_t MAX_TIMERS = 10;
+
+  Timer();
+
+  int8_t setTimeout(void (*function)(), unsigned long timeout);
+  int8_t setInterval(void (*function)(), unsigned long interval);
+  void deleteTimer(uint8_t slot);
+  void update();
+
+private:
+  int8_t findTimerSlot();
+
+  void (*functions[MAX_TIMERS])();
+  unsigned long intervals[MAX_TIMERS];
+  unsigned long currentIntervals[MAX_TIMERS];
+  bool repeat[MAX_TIMERS];
+
+  unsigned long lastUpdate;
+};
+
+#endif
