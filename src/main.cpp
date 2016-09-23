aac2916796fa8c1ff1b435687f676e3c1d32fd22
Dominik Toton <vaaadus@gmail.com>
2016-09-23 22:55:55 +0200

    Fixed bugs

--- src/main.cpp
+++ src/main.cpp
@@ -8,7 +8,7 @@ LiquidCrystal *lcd;
 TempSensor *tempSensor;
 Timer timer;
 
-float tz, tkw, tskw, tco, tb, tcwu, tkg;
+float tz, tkw, tskw, tco, tb, tcwu, tkg, tp;
 
 void initPins();
 void initLcd();
@@ -19,6 +19,7 @@ void prepareAndReadTSKW();
 void readTSKW();
 void readTKG();
 void controlZT();
+
 void tempSensorError(const char *name, const float temp);
 
 void updateDisplay();
@@ -29,10 +30,15 @@ void printTCO();
 void printTB();
 void printTZ();
 void printTKG();
+void printTP();
 
-bool shouldTurnOnPKW() { return tkw > 40 && tskw > 100; }
-bool shouldTurnOffPKW() { return tskw < 100; }
-bool shouldTurnOnPCWU() { return (tb > tcwu + 5) && (tcwu < 60); }
+inline bool shouldTurnOnPKW() { return tkw >= 40 && tskw >= 100; }
+inline bool shouldTurnOffPKW() { return tskw <= 100; }
+inline bool shouldTurnOnPCWU() { return (tb >= tcwu + 5) && (tcwu < 65); }
+inline bool shouldTurnOffPCWU() { return tb <= tcwu + 2; }
+inline bool shouldTurnOnPCO() { return tz < 17; }
+inline bool shouldOpenZT() { return tco <= tkg - 1.5; }
+inline bool shouldCloseZT() { return tco >= tkg + 1.5; }
 
 int main() {
   initPins();
@@ -41,7 +47,7 @@ int main() {
 
   timer.setInterval(readTemperatures, 2000);
   timer.setInterval(prepareAndReadTSKW, 5000);
-  timer.setInterval(readTKG, 60000);
+  timer.setInterval(readTKG, 600000);  // 10min.
   timer.setInterval(controlZT, 10000); // min 5.1s
   timer.setInterval(updateDisplay, 1000);
 
@@ -59,8 +65,14 @@ int main() {
 
     if (shouldTurnOnPCWU()) {
       digitalWrite(PCWU_PIN, HIGH);
-    } else {
+    } else if (shouldTurnOffPCWU()) {
       digitalWrite(PCWU_PIN, LOW);
+    }
+
+    if (shouldTurnOnPCO()) {
+      digitalWrite(PCO_PIN, HIGH);
+    } else {
+      digitalWrite(PCO_PIN, LOW);
     }
 
     timer.update();
@@ -74,13 +86,14 @@ void initPins() {
   pinMode(TSKW_POWER_PIN, OUTPUT);
   pinMode(PKW_PIN, OUTPUT);
   pinMode(PCWU_PIN, OUTPUT);
+  pinMode(PCO_PIN, OUTPUT);
   pinMode(ZTZ_PIN, OUTPUT);
   pinMode(ZTC_PIN, OUTPUT);
 }
 
 void initLcd() {
-  lcd = new LiquidCrystal(RS, ENABLE, D4, D5, D6, D7);
-  lcd->begin(16, 2);
+  lcd = new LiquidCrystal(RS, RW, ENABLE, D0, D1, D2, D3, D4, D5, D6, D7);
+  lcd->begin(20, 4);
   lcd->clear();
   lcd->noBlink();
   lcd->noCursor();
@@ -93,6 +106,7 @@ void initTempSensor() {
   tempSensor->setTCOAddress(TCOAddress);
   tempSensor->setTBAddress(TBAddress);
   tempSensor->setTCWUAddress(TCWUAddress);
+  tempSensor->setTPAddress(TPAddress);
 }
 
 void readTemperatures() {
@@ -101,17 +115,20 @@ void readTemperatures() {
   tco = tempSensor->readTCO();
   tb = tempSensor->readTB();
   tcwu = tempSensor->readTCWU();
+  tp = tempSensor->readTP();
 
   if (!tempSensor->isTZInRange(tz))
-    tempSensorError("TZ", tz);
+    tempSensorError("Tz", tz);
   if (!tempSensor->isTKWInRange(tkw))
-    tempSensorError("TKW", tkw);
+    tempSensorError("Tkw", tkw);
   if (!tempSensor->isTCOInRange(tco))
-    tempSensorError("TCO", tco);
+    tempSensorError("Tco", tco);
   if (!tempSensor->isTBInRange(tb))
-    tempSensorError("TB", tb);
+    tempSensorError("Tb", tb);
   if (!tempSensor->isTCWUInRange(tcwu))
-    tempSensorError("TCWU", tcwu);
+    tempSensorError("Tcwu", tcwu);
+  if (!tempSensor->isTPInRange(tp))
+    tempSensorError("Tp", tp);
 }
 
 void prepareAndReadTSKW() {
@@ -128,8 +145,8 @@ void readTSKW() {
 }
 
 void readTKG() {
-  const double KG = 1.1;  // nachylenie krzywej grzania
-  const double TW = 21.5; // pożądana temperatura wewnątrz
+  const double KG = 1.0; // nachylenie krzywej grzania
+  const double TW = 21;  // pożądana temperatura wewnątrz
   tkg = KG * (TW - tz) + TW;
 }
 
@@ -137,10 +154,10 @@ void openZTCEnd() { digitalWrite(ZTC_PIN, LOW); }
 void openZTZEnd() { digitalWrite(ZTZ_PIN, LOW); }
 
 void controlZT() {
-  if (tco < tkg) {
+  if (shouldOpenZT()) {
     digitalWrite(ZTC_PIN, HIGH);
     timer.setTimeout(openZTCEnd, 3100); // 1 st.
-  } else {
+  } else if (shouldCloseZT()) {
     digitalWrite(ZTZ_PIN, HIGH);
     timer.setTimeout(openZTZEnd, 5100); // 5/3 st.
   }
@@ -170,51 +187,52 @@ void updateDisplay() {
   printTB();
   printTZ();
   printTKG();
+  printTP();
 }
 
 void printTSKW() {
   lcd->setCursor(0, 0);
-  lcd->print("TSKW ");
+  lcd->print("Tskw ");
   lcd->print((int)tskw);
 }
 
 char buff[10];
 void formatFloat(const float temp) {
-  sprintf(buff, "%d.%d", abs((int)tkw), abs((int)(tkw * 10) % 10));
+  sprintf(buff, "%d.%d", abs((int)temp), abs((int)(temp * 10) % 10));
 }
 
 void printTKW() {
   formatFloat(tkw);
   lcd->setCursor(10, 0);
-  lcd->print("TKW ");
+  lcd->print("Tkw ");
   lcd->print(buff);
 }
 
 void printTCWU() {
   formatFloat(tcwu);
   lcd->setCursor(0, 1);
-  lcd->print("TCWU ");
+  lcd->print("Tcwu ");
   lcd->print(buff);
 }
 
 void printTCO() {
   formatFloat(tco);
   lcd->setCursor(10, 1);
-  lcd->print("TCO ");
+  lcd->print("Tco ");
   lcd->print(buff);
 }
 
 void printTB() {
   formatFloat(tb);
-  lcd->setCursor(2, 0);
-  lcd->print("TB   ");
+  lcd->setCursor(0, 2);
+  lcd->print("Tb   ");
   lcd->print(buff);
 }
 
 void printTZ() {
   formatFloat(tz);
-  lcd->setCursor(2, 10);
-  lcd->print("TZ ");
+  lcd->setCursor(10, 2);
+  lcd->print("Tz ");
   if (tz < 0) {
     lcd->print("-");
   } else {
@@ -225,7 +243,14 @@ void printTZ() {
 
 void printTKG() {
   formatFloat(tkg);
-  lcd->setCursor(3, 0);
-  lcd->print("TKG  ");
+  lcd->setCursor(0, 3);
+  lcd->print("Tkg  ");
+  lcd->print(buff);
+}
+
+void printTP() {
+  formatFloat(tp);
+  lcd->setCursor(10, 3);
+  lcd->print("Tp  ");
   lcd->print(buff);
 }
